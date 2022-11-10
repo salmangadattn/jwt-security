4ab09c8512695b67093a7573bf765475b55bd02a
salmangadattn <salman.gada@tothenew.com>
2022-11-10 19:32:50 +0530

    initial commit

---
+++ src/main/java/com/ecommerce/bigbasket/BigBasketApplication.java
@@ -0,0 +1,13 @@
+package com.ecommerce.bigbasket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BigBasketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BigBasketApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ecommerce/bigbasket/BigBasketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ecommerce.bigbasket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BigBasketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
